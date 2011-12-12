878b2d30c1e6597d7b26c1badd4fef33cfb545ca
Astro <astro@spaceboyz.net>
2011-12-12 17:19:35 +0100

    test_parse: text with commas

--- test/test_parse.js
+++ test/test_parse.js
@@ -9,6 +9,10 @@ vows.describe('ltx').addBatch({
             assert.equal(el.name, 'root');
             assert.equal(0, el.children.length);
         },
+	'text with commas': function() {
+	    var el = ltx.parse("<body>sa'sa'1'sasa</body>");
+	    assert.equal("sa'sa'1'sasa", el.getText());
+	},
         'erroneous document raises error': function() {
             assert.throws(function() {
                 ltx.parse('<root></toor>');
