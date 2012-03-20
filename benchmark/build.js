584e889c94b4c8d86e64abd2b6d471dfec7ee9f4
Astro <astro@spaceboyz.net>
2012-03-20 01:57:57 +0100

    proper browser benchmark building

--- benchmark/build.js
+++ benchmark/build.js
@@ -1,10 +1,13 @@
 var browserify = require('browserify');
 var path = require('path');
 var b = browserify({
-    debug: true,
-    require: ["ltx/lib/sax_saxjs.js", "ltx/lib/sax_easysax.js"]
+    debug: true
 });
-b.addEntry('../benchmark/browser_benchmark.js');
+var root = path.join(__dirname, "..");
+b.require("./lib/index-browserify.js",
+    { root: root, basedir: root });
+b.alias("ltx", "./lib/index-browserify.js");
+b.addEntry('browser_benchmark.js');
 
 var fs = require('fs');
 
