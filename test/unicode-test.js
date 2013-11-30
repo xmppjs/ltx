09528e4feb5dad15938bd1fa6b23feeaf805392b
▟ ▖▟ ▖ <dodo.the.last@gmail.com>
2013-11-30 22:27:58 +0100

    update #29 test with data from node-xmp/node-xmpp#198

--- test/unicode-test.js
+++ test/unicode-test.js
@@ -17,6 +17,18 @@ vows.describe('unicode').addBatch({
            )
                .c('body').t('니코드<'.toString('utf8'))
            assert.equal(element.getChildText('body'), text)
+        },
+        'issue-29 test write': function() {
+            var text = '유니코드'
+            var result = '<0message0 0to0="0-1@chat.fb.com0"0 0type0="0chat0"0>0<0body0>'.split(0)
+            result.push(text)
+            result = result.concat('</0body0>0</0message0>'.split(0))
+            var element = new Element('message', {to:'-1@chat.fb.com', type:'chat'})
+            element.c('body').t(text.toString('utf8'))
+            element.write(function(c) {
+                assert.equal(result.shift(), c)
+            })
+            assert.equal(result.length, 0)
         }
     }
 }).export(module)
