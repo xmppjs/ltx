890f5862d5212f81204964180c6d5d798d9be8d8
▟ ▖▟ ▖ <dodo.the.last@gmail.com>
2013-11-30 22:31:53 +0100

    issue-29 test #3 which is not failing

--- test/unicode-test.js
+++ test/unicode-test.js
@@ -18,6 +18,14 @@ vows.describe('unicode').addBatch({
                .c('body').t('니코드<'.toString('utf8'))
            assert.equal(element.getChildText('body'), text)
         },
+        'issue-29 test #3': function() {
+           var text = '니코드<'
+           var element = new Element(
+               'message', { to: 'you@server.com', type: 'chat' }
+           )
+               .c('body').t('니코드<'.toString('utf8'))
+           assert.equal(element.getText(), text)
+        },
         'issue-29 test write': function() {
             var text = '유니코드'
             var result = '<0message0 0to0="0-1@chat.fb.com0"0 0type0="0chat0"0>0<0body0>'.split(0)
