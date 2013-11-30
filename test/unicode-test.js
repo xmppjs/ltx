c15e0a068abc3d049e5bfb82bca7dc1fcdec01f4
Lloyd Watkin <lloyd@evilprofessor.co.uk>
2013-11-30 21:10:05 +0000

    Add failing test for issue #29

--- test/unicode-test.js
+++ test/unicode-test.js
@@ -9,6 +9,14 @@ vows.describe('unicode').addBatch({
             var element = new Element('iq')
             element.t(text)
             assert.equal(element.toString(), '<iq>' + text + '</iq>')
+        },
+        'issue-29 test #2': function() {
+           var text = '니코드<'
+           var element = new Element(
+               'message', { to: 'you@server.com', type: 'chat' }
+           )
+               .c('body').t('니코드<'.toString('utf8'))
+           assert.equal(element.getChildText('body'), text)
         }
     }
 }).export(module)
