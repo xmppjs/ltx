6ec0417ada4ce5a86b7dc24c31906e786dfbbfd0
Sonny Piers <sonny@fastmail.net>
2019-10-02 02:34:37 +0200

    fix cdata issue #132 (#133)

--- test/cdata-test.js
+++ test/cdata-test.js
@@ -4,6 +4,7 @@ var vows = require('vows')
 var assert = require('assert')
 var ltx = require('..')
 var parsers = require('../lib/parsers')
+var h = ltx.createElement
 
 var Parser = parsers.find(function (parser) {
   return (parser.name === 'SaxLtx')
@@ -24,6 +25,10 @@ vows.describe('sax_ltx').addBatch({
       var el = parse('<root><![CDATA[Content &amp; "more content&quot;]]></root>')
       assert.strictEqual(el.name, 'root')
       assert.strictEqual(el.getText(), 'Content &amp; "more content&quot;')
+    },
+    'issue-132': () => {
+      var el = parse('<a><b><![CDATA[]]></b><b><![CDATA[--><c>&d;]]></b></a>')
+      assert.deepStrictEqual(el, h('a', null, h('b', null, ''), h('b', null, '--><c>&d;')))
     }
   }
 }).export(module)
