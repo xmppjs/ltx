fa5a95c9b508f87a57183ad62f335146c6a4a00b
Sonny Piers <sonny@fastmail.net>
2024-12-12 01:19:58 +0100

    Fix dual ESM/CJS export
    Fixes https://github.com/xmppjs/ltx/issues/156

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,6 @@
 export default [
   {
-    input: [
-      "src/ltx.js",
-      "src/DOMElement",
-      "src/parsers.js",
-      "src/parsers/libxmljs.js",
-    ],
+    input: ["src/ltx.js", "src/parsers.js"],
     output: {
       dir: "lib",
       format: "cjs",
